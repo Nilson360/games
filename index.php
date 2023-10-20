7de849513c3d78e7426927a5d16248b687f9ea98
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-10-20 19:54:28 +0200

    add filees

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <div id="body">
+        <p>Escolha o seu jogo</p>
+        <table class="listagem">
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+            <tr><td>Foto</td><td>Nome</td><td>Adm</td></tr>
+        </table>
+    </div>
+</body>
+</html>
